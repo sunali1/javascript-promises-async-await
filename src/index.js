2c4531d1ab103363b5f0dab322417eed38d27092
Sunali Seth <sunaliseth01@gmail.com>
2021-05-31 23:38:31 +0100

    Return results with moviePromise

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-export const fetchMovies = () => {};
+import {fetchWithTimeout} from './services';
+
+
+const movies = require('./data/movies.json');
+
+export const fetchMovies = () => {
+  const resolveFunction = () => movies;
+  return fetchWithTimeout(1000).then(resolve(resolveFunction));
+};
+
+const moviePromise = fetchMovies().then((results => {return console.log(results)}));
+
+
+// const movies = require('./data/movies.json');
+
+// export const fetchMovies = () => {
+//     const resolveFunction = () => movies;
+//     fetchWithTimeout(1000).then((resolve) => resolve(resolveFunction));
+// };
+
+// const moviePromise = fetchMovies().then((results => console.log(results)));
