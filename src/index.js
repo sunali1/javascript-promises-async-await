788483591af8db857403741989a635f597195fde
Sunali Seth <sunaliseth01@gmail.com>
2021-06-02 10:49:49 +0100

    Remove semicolon after fetchMovies function

--- src/index.js
+++ src/index.js
@@ -6,19 +6,9 @@ const movies = require('./data/movies.json');
 export function fetchMovies() {
   const resolveFunction = () => movies;
   return fetchWithTimeout(1000).then(resolveFunction);
-};
+}
 
 const moviePromise = fetchMovies();
-
 moviePromise.then(results => {
     console.log(results);
 });
-
-// const movies = require('./data/movies.json');
-
-// export const fetchMovies = () => {
-//     const resolveFunction = () => movies;
-//     fetchWithTimeout(1000).then((resolve) => resolve(resolveFunction));
-// };
-
-// const moviePromise = fetchMovies().then((results => console.log(results)));
