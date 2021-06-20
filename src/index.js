d3ddfb1496cccf9a8a22bb31014cd05fe381552b
Sunali Seth <sunaliseth01@gmail.com>
2021-06-20 23:18:22 +0100

    Add getMovies&Books getMoviesorBooks promises to index

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import {fetchBooks, fetchMovies} from './services';
+import {fetchBooks, fetchMovies, asyncFetchBooks, asyncFetchMovies} from './services';
 
 function getBooksAndMovies(){
  return Promise.all([fetchBooks(), fetchMovies()])
@@ -20,3 +20,34 @@ function getBooksOrMovies(){
 const getBooksOrMoviesPromise = getBooksOrMovies();
 getBooksOrMoviesPromise.then(results => {console.log('getBooksOrMoviesPromise', results)});
 
+async function getBooksAndMoviesAsync () {
+  try {
+    const [books, movies] = await Promise.all([asyncFetchBooks(), asyncFetchMovies()])
+    return {books, movies};
+  } catch (error) {
+    console.log("Error fetching books and movies", error)
+  }
+};
+
+async function getBooksOrMoviesAsync(){
+  try {
+    const values = await Promise.race([asyncFetchBooks(), asyncFetchMovies()]);
+    return values;
+  } catch (error) {
+    console.error("Error waiting for the promise race", error);
+  }
+};
+
+getBooksAndMoviesAsync().then(results => {
+  console.log("movies and books", {
+    movies: results.movies,
+    books: results.books
+  });
+});
+
+
+getBooksOrMoviesAsync().then(results => {
+  console.log("movies OR books", {
+    results
+  });
+})
